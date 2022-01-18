1e40b290d6ef85a0cddaaaad142a7dd6e18e0fc2
SaiChaithrikaVasani <96117347+SaiChaithrikaVasani@users.noreply.github.com>
2022-01-18 21:56:36 +0530

    added bookHistory method

--- AccountMaintenance.java
+++ AccountMaintenance.java
@@ -129,5 +129,53 @@ public class AccountMaintenance {
             fos.close();
         }
     }
+        /**
+     * 
+     * @throws Exception
+     * This method displays the books issued to the User along with Date of Issue
+     * It also displays the books returned by the user along with the Date of Return
+     * This method throws FileNotFoundException if the file is not available
+     * FileName : BookTransactionData.csv
+     */
 
+    public  void booksHistory() throws Exception{
+         
+        File file = new File("BookTransactionData.csv");
+        Scanner scan = new Scanner(file);
+        scan.useDelimiter("\n");
+        while(scan.hasNext()){
+            String temp = scan.next().trim();
+            String[] data = temp.split(",");
+            if(data[0].equals(this.email) && data.length >=1){
+                int issued = 0;
+                int returned = 0;
+                if(data.length>=2){
+                    String[] booksIssued = data[1].split(";");
+                    System.out.println("Books issued are");
+                    System.out.println("Book name  Issued Date");
+                    for(String a :booksIssued){
+                        System.out.println(a);
+                        issued =1;
+                    }
+                }
+                if(issued ==0){
+                    System.out.println("No books Issued");
+                }
+                if(data.length ==3){
+                    String[] booksReturned = data[2].split(";");
+                    System.out.println("Books returned are");
+                    System.out.println("Book name  Returned Date");
+                    for(String a :booksReturned){
+                        System.out.println(a);
+                        returned =1;
+                    }
+                }
+                if(returned ==0){
+                    System.out.println("No books returned");
+                }
+            }
+           
+        }
+        scan.close();
+    }
 }
