a24279d312a2919f35e44dd36cbaa06a2ca942b5
Snehitha <95093519+PSnehitha@users.noreply.github.com>
2022-01-26 19:23:02 +0530

    Add files via upload

---
+++ Search.java
@@ -0,0 +1,129 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Search {
+    /**
+     * This method takes the title of the book as the parameter to search whether the book is available or not  
+     * @param title title of the book to be checked
+     * @throws Exception
+     * filename: booklist.csv
+     */
+
+    public static void searchByTitle(String title) throws Exception{
+        File file = new File("booklist.csv");
+        Scanner scan = new Scanner(file);
+        int searchResult = 0;
+        scan.useDelimiter("\n");
+        while(scan.hasNext()){
+            String temp = scan.next();
+            if(!temp.equals("")){
+                String[] details = temp.split(",");
+                if(details[0].equalsIgnoreCase(title)){
+                    String bookdetails ="\n"+"Book name :"+details[0]+"\n"+"Author name :"+details[1]+"\n"+"ISBN number :"+details[2]+"\n"+"Publisher :"+details[3];
+                    System.out.println(bookdetails);
+                    searchResult =1;
+                    break;
+                }
+            }
+        }
+        if(searchResult == 0){
+            System.out.println("Book is not available");
+        }
+        scan.close();
+
+    }
+    /**
+     * This method takes the author of the book as parameter to search whether the book is available or not
+     * @param author author of the book to be checked
+     * @throws Exception
+     * filename: booklist.csv
+     */
+    public static void searchByAuthor(String author) throws Exception{
+        File file = new File("booklist.csv");
+        Scanner scan = new Scanner(file);
+        int searchResult = 0;
+        scan.useDelimiter("\n");
+        while(scan.hasNext()){
+            String temp = scan.next();
+            if(!temp.equals("")){
+                String[] details = temp.split(",");
+                if(details[1].equalsIgnoreCase(author)){
+                    String bookdetails ="\n"+"Book name :"+details[0]+"\n"+"Author name :"+details[1]+"\n"+"ISBN number :"+details[2]+"\n"+"Publisher :"+details[3];
+                    searchResult =1;
+                    System.out.println(bookdetails);
+                    break;
+                }
+            }
+           
+
+        }
+        if(searchResult == 0){
+            System.out.println("Book is not available");
+        }
+        scan.close();
+        
+
+    }
+    /**
+     * This method takes the isbn number of the book as parameter to search whether the book is available or not
+     * @param isbn isbn number of the book to be checked
+     * @throws Exception
+     * filename: booklist.csv
+     */
+
+    public static void searchByISBN(String isbn) throws Exception{
+        File file = new File("booklist.csv");
+        Scanner scan = new Scanner(file);
+        int searchResult = 0;
+        scan.useDelimiter("\n");
+        while(scan.hasNext()){
+            String temp = scan.next();
+            if(!temp.equals("")){
+                String[] details = temp.split(",");
+                if(details[2].equalsIgnoreCase(isbn)){
+                    String bookdetails ="\n"+"Book name :"+details[0]+"\n"+"Author name :"+details[1]+"\n"+"ISBN number :"+details[2]+"\n"+"Publisher :"+details[3];
+                    searchResult =1;
+                    System.out.println(bookdetails);
+                    break;
+                }
+            }
+        }
+        if(searchResult == 0){
+            System.out.println("Book is not available");
+        }
+        scan.close();
+    }
+    /**
+     * This method takes the author of the book as parameter to search whether the book is available or not
+     * @param publisher publisher of the book to be checked
+     * @throws Exception
+     * filename: booklist.csv
+     */
+
+    public static void searchByPublisher(String publisher) throws Exception{
+        File file = new File("booklist.csv");
+        Scanner scan = new Scanner(file);
+        int searchResult = 0;
+        scan.useDelimiter("\n");
+        while(scan.hasNext()){
+            String temp = scan.next();
+            if(!temp.equals("")){
+                String[] details = temp.split(",");
+                if(details[3].equalsIgnoreCase(publisher)){
+                    String bookdetails ="\n"+"Book name :"+details[0]+"\n"+"Author name :"+details[1]+"\n"+"ISBN number :"+details[2]+"\n"+"Publisher :"+details[3];
+                    searchResult =1;
+                    System.out.println(bookdetails);
+                    break;
+                }
+            }
+            
+
+        }
+        if(searchResult == 0){
+            System.out.println("Book is not available");
+        }
+        scan.close();
+
+    }
+}
+
